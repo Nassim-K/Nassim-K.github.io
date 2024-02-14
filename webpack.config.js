abf36f33cc2e07c412d6578668551275b30f07a9
Nassim <43495536+Nassim-K@users.noreply.github.com>
2024-02-14 19:54:27 +0100

    Start building portfolio

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: './assets/js/app.js',
+  mode: 'development',
+  watch: true,
+  output: {
+    path: `${__dirname}/public/build`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: 'css/',
+              name: '[name].min.css'
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+};
